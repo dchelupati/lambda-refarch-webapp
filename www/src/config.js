e0cd4bab2d1a22e2d96d975952c7aea96867ba2b
dchelupati <62074409+dchelupati@users.noreply.github.com>
2021-05-14 16:54:19 -0700

    Add files via upload

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "2eo2bsf7t07lv4vtrh2hieod9v",     // CognitoClientID
+  "api_base_url": "https://um9cbfkz2f.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-testtodoapp.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://localhost:8080"                                    // AmplifyURL
 };
 
 export default config;
