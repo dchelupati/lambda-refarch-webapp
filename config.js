63e08665f28c470000a51afa1cacacb9d50ef84d
dchelupati <62074409+dchelupati@users.noreply.github.com>
2021-05-18 12:15:54 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "2eo2bsf7t07lv4vtrh2hieod9v",     // CognitoClientID
-  "api_base_url": "https://um9cbfkz2f.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "api_base_url": "https://um9cbfkz2f.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-testtodoapp.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d11h0d8d6sddez.amplifyapp.com"
   // AmplifyURL
